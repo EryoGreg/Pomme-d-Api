add4829b03ad0b3a6c4ca346c1adea64e41682ee
Unknown <elouan.dz@gmail.com>
2018-02-16 17:14:57 +0100

    gestion d'erreures
    + try catch
    + ascii art

--- api.js
+++ api.js
@@ -1,13 +1,24 @@
 const request = require('request-promise-native');
 
 module.exports.getCityWeather = async function(city, { apiKey }){
-    let response = await request(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=${apiKey}&units=metric`);
-    response = JSON.parse(response);
-    return response
+    try{
+        let response = await request(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&APPID=${apiKey}&units=metric`);
+        response = JSON.parse(response);
+        return response
+    }catch(err) {
+        console.error('Erreur de connection à l\'api ')
+    }
+
+
 };
 
 module.exports.getWeather = async function ({apiKey}) {
-    let response = await request(`http://api.openweathermap.org/data/2.5/weather?q=Bordeaux&lang=fr&APPID=${apiKey}&units=metric`);
-    response = JSON.parse(response);
-    return response
+    try{
+        let response = await request(`http://api.openweathermap.org/data/2.5/weather?q=Bordeaux&lang=fr&APPID=${apiKey}&units=metric`);
+        response = JSON.parse(response);
+        return response
+    }catch (err){
+
+    }
+
 };
--- index.js
+++ index.js
@@ -5,12 +5,30 @@ const api = require('./api');
 const settings = require('./settings.json');
 
 let city = "Bordeaux";
+
+welcomeFunction();
+
+function welcomeFunction() {
+    console.log(
+        ' ____  _____  __  __  __  __  ____    ____/   __    ____  ____ \n' +
+        '(  _ \\(  _  )(  \\/  )(  \\/  )( ___)  (  _ \\  /__\\  (  _ \\(_  _)\n' +
+        ' )___/ )(_)(  )    (  )    (  )__)    )(_) )/(__)\\  )___/ _)(_ \n' +
+        '(__)  (_____)(_/\\/\\_)(_/\\/\\_)(____)  (____/(__)(__)(__)  (____)\n' +
+        '\n');
+}
+
 api.getCityWeather(city, settings).then((data)=> {
-    console.log("les pommes de "+city+" subissent un climat de type "+data.weather[0].description); // pluie fine /
+    // si on reçoit de la data :
+    if (data){
+        console.log("les pommes de "+city+" subissent un climat de type "+data.weather[0].description); // pluie fine /
+    }
 }).catch((err)=> console.log(err));
 
 api.getWeather(settings).then((data)=> {
-    console.log("les pommes ont trop de "+data.weather[0].description); // pluie fine /
+    // si on reçoit de la data :
+    if (data){
+        console.log("les pommes ont trop de "+data.weather[0].description); // pluie fine /
+    }
 }).catch((err)=> console.log(err));
 
 
