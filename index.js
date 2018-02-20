a4ae98b1ec09eaf3d07692fc6f1ea90a9d550100
Unknown <elouan.dz@gmail.com>
2018-02-20 16:51:05 +0100

    async + inquirer

--- index.js
+++ index.js
@@ -4,18 +4,83 @@
 const api = require('./api');
 const settings = require('./settings.json');
 
+const inquirer = require('inquirer');
+
 let city = "Bordeaux";
 
 welcomeFunction();
 
 function welcomeFunction() {
-    console.log(
-        ' ____  _____  __  __  __  __  ____    ____/   __    ____  ____ \n' +
-        '(  _ \\(  _  )(  \\/  )(  \\/  )( ___)  (  _ \\  /__\\  (  _ \\(_  _)\n' +
-        ' )___/ )(_)(  )    (  )    (  )__)    )(_) )/(__)\\  )___/ _)(_ \n' +
-        '(__)  (_____)(_/\\/\\_)(_/\\/\\_)(____)  (____/(__)(__)(__)  (____)\n' +
-        '\n');
+    return new Promise(async function (resolve, reject) {
+        console.log(
+            ' ____  _____  __  __  __  __  ____    ____/   __    ____  ____ \n' +
+            '(  _ \\(  _  )(  \\/  )(  \\/  )( ___)  (  _ \\  /__\\  (  _ \\(_  _)\n' +
+            ' )___/ )(_)(  )    (  )    (  )__)    )(_) )/(__)\\  )___/ _)(_ \n' +
+            '(__)  (_____)(_/\\/\\_)(_/\\/\\_)(____)  (____/(__)(__)(__)  (____)\n' +
+            '\n');
+
+        let answer = await inquirer.prompt([
+            {
+                type: 'list',
+                message: 'Aimez vous les pommes ?',
+                name: 'Pommes',
+                choices: [
+                    'j\'adore !!',
+                    'pas du tout'
+                ]
+            }, {
+                type: 'checkbox',
+                message: 'De quelles pommes parlez vous ?',
+                name: 'city',
+                choices: [
+                    new inquirer.Separator('Les villes :'),
+                    'Bordeaux',
+                    'Paris',
+                    'London',
+                    'Berlin',
+                    'Madrid',
+                    new inquirer.Separator('ajouter une ville'),
+                    'Autre ville',
+                ]
+            }
+        ]);
+        console.log(answer);
+        if (answer.city.includes('Autre ville')) { //answer contains AutreVille
+            const customCityObj = await inquirer.prompt([
+                {
+                    type: 'input',
+                    message: 'Entrez le nom de la ville',
+                    name: 'city',
+                    validate: function (value) { // que des lettres
+                        var pass = value.match(
+                            /^[a-zA-Z]{2,}/
+                        );
+                        if (pass) {
+                            return true;
+                        }
+                        return 'entrez une ville d\'au moins 2 lettres : ';
+                    }
+                }
+            ]);
+
+            answer = await jsonConcat(answer, customCityObj);
+
+            console.log(answer);
+        }
+    }).catch(err => console.log(err));
 }
+
+function jsonConcat(answers, customCityObj) {
+    for (let key in answers.city) { // pour chaque ville selectionnée
+        if (answers.city[key] == "Autre ville") {
+            answers.city[key] = customCityObj.city
+        }
+
+    }
+    return answers;
+}
+
+/*
 
 api.getCityWeather(city, settings).then((data)=> {
     // si on reçoit de la data :
@@ -30,5 +95,73 @@ api.getWeather(settings).then((data)=> {
         console.log("les pommes ont trop de "+data.weather[0].description); // pluie fine /
     }
 }).catch((err)=> console.log(err));
+*/
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ce que je veux faire :
+/*
+
+var x = fonction {
+return toto
+}
+
+if x = toto
+     x= tata
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
